d812dcfed42eddd859e88e2bc209f83c71bf32d4
Oopsse <92084378+Oopsse@users.noreply.github.com>
2025-10-03 16:10:27 +0200

    Update install.sh
    sh -c "$(curl -fsSL https://raw.githubusercontent.com/Oopsse/setup/master/install.sh)"

--- install.sh
+++ install.sh
@@ -2,14 +2,21 @@
 
 # Installation
 
-KERNEL="linux-image-6.16.3+deb13-amd64"
-LUKS="plymouth plymouth-themes"
-DM="sddm sddm-theme-breeze"
-WM="xinit i3 xsecurelock"
-SYSTEM="make file tree pasystray arandr krb5-user sudo dunst libnotify-bin iotop usbutils inxi acpi firmware-linux-free lsb-release dbus dbus-x11 systemd-timesyncd"
-NETWORK="blueman network-manager-applet"
-UTILS="feh flameshot terminator openssh-client neovim git curl wget ca-certificates dnsutils resolvconf xclip ncdu x11-utils rofi make htop chromium firefox-esr-l10n-fr"
-DISK="dolphin gvfs-fuse gvfs-backends ntfs-3g exfatprogs exfat-fuse dosfstools partitionmanager gdisk"
+KERNEL="linux-image-6.16.3+deb13-amd64"    
+LUKS="plymouth plymouth-themes"    
+DM="sddm sddm-theme-breeze"    
+WM="xinit i3 xsecurelock"    
+SYSTEM="make file fish tree pasystray arandr krb5-user sudo dunst libnotify-bin iotop usbutils inxi acpi firmware-linux-free lsb-release dbus dbus-x11 systemd-timesyncd"    
+NETWORK="blueman network-manager-applet"    
+UTILS="feh flameshot terminator openssh-client neovim git curl wget ca-certificates dnsutils resolvconf xclip ncdu x11-utils rofi make htop chromium firefox-esr-l10n-fr"    
+DISK="dolphin gvfs-fuse gvfs-backends ntfs-3g exfatprogs exfat-fuse dosfstools partitionmanager gdisk"    
+    
+## APT    
+echo 'deb http://deb.debian.org/debian/ trixie-backports main non-free-firmware' | tee /etc/apt/sources.list    
+echo 'deb-src http://deb.debian.org/debian/ trixie-backports main non-free-firmware' | tee /etc/apt/sources.list    
+echo 'deb http://download.opensuse.org/repositories/shells:/fish:/release:/4/Debian_13/ /' | tee /etc/apt/sources.list.d/shells:fish:release:4.list         
+curl -fsSL https://download.opensuse.org/repositories/shells:fish:release:4/Debian_13/Release.key | gpg --dearmor | tee /etc/apt/trusted.gpg.d/shells_fish_release_4.gpg > /dev/null         
+sudo apt update    
 
 ## Kernel
 apt install -y $KERNEL
