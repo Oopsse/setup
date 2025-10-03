f11f85baeae3ac5e7db50f90426b9e674428a7a7
Oopsse <92084378+Oopsse@users.noreply.github.com>
2025-10-03 16:02:20 +0200

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,53 @@
+#!/bin/bash
+
+# Installation
+
+KERNEL="linux-image-6.16.3+deb13-amd64"
+LUKS="plymouth plymouth-themes"
+DM="sddm sddm-theme-breeze"
+WM="xinit i3 xsecurelock"
+SYSTEM="make file tree pasystray arandr krb5-user sudo dunst libnotify-bin iotop usbutils inxi acpi firmware-linux-free lsb-release dbus dbus-x11 systemd-timesyncd"
+NETWORK="blueman network-manager-applet"
+UTILS="feh flameshot terminator openssh-client neovim git curl wget ca-certificates dnsutils resolvconf xclip ncdu x11-utils rofi make htop chromium firefox-esr-l10n-fr"
+DISK="dolphin gvfs-fuse gvfs-backends ntfs-3g exfatprogs exfat-fuse dosfstools partitionmanager gdisk"
+
+## Kernel
+apt install -y $KERNEL
+
+## Luks
+apt install -y $LUKS
+
+## Display Manager
+apt install --no-install-recommends $DM
+
+## wms & x 
+apt install -y $WM
+
+## System
+apt install -y $SYSTEM
+
+## Network
+apt install -y $NETWORK
+
+## File manager & Disk & File system
+apt install -y $DISK
+
+## Utilitaire
+apt install -y $UTILS
+
+# Configuration
+## LUKS
+### Configure /usr/share/plymouth/plymouthd.defaults to tribar and sudo update-initramfs -u
+sed -i 's/Theme=.*$/Theme=tribar/' /usr/share/plymouth/plymouthd.defaults
+update-initramfs -u
+## Grub
+sed -i 's/GRUB_TIMEOUT=.*$/GRUB_TIMEOUT=0/' /etc/default/grub
+sed -i 's/GRUB_CMDLINE_LINUX_DEFAULT=.*$/GRUB_CMDLINE_LINUX_DEFAULT="quiet splash"/' /etc/default/grub
+update-grub
+## SDDM
+wget "https://images2.alphacoders.com/117/1171867.png" -o "/usr/share/wallpapers/Next/contents/images/1.png"
+chown root:root "/usr/share/wallpapers/Next/contents/images/1.png"
+chmod 644 "/usr/share/wallpapers/Next/contents/images/1.png"
+
+### Xsecurelock
+##
