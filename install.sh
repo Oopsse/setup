287965adc1d3152590e41e1e86fabaf3917a335e
Oopsse <92084378+Oopsse@users.noreply.github.com>
2025-10-09 12:13:17 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Installation
-# sh -c "$(wget https://raw.githubusercontent.com/Oopsse/setup/master/install.sh -O -)"
+# sh -c "$(wget https://raw.githubusercontent.com/Oopsse/setup/main/install.sh -O -)"
 
 export DEBIAN_FRONTEND=noninteractive
 
@@ -27,7 +27,7 @@ apt install -y $KERNEL
 apt install -y $LUKS
 
 ## Display Manager
-apt install --no-install-recommends $DM
+apt install --no-install-recommends -y $DM
 
 ## wms & x 
 apt install -y $WM
